605fbf59861fc3e8b9dddd9ef2528defbc81260a
Michael L Perry <michael@qedcode.com>
2022-12-03 22:16:39 -0600

    Lay out the room detail screen.

--- rooms/RoomDetailScreen.tsx
+++ rooms/RoomDetailScreen.tsx
@@ -1,6 +1,7 @@
 import { ErrorScreen } from "@components/ErrorScreen";
 import { LoadingScreen } from "@components/LoadingScreen";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
+import { Avatar } from "react-native-paper";
 import { StackContainerScreenProps } from "../navigation/StackContainerParamList";
 import { useRoom } from "./queries";
 
@@ -22,10 +23,33 @@ export const RoomDetailScreen = ({ navigation, route }: RoomDetailScreenProps) =
     return <LoadingScreen />;
   }
 
+  if (!data.guest) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.label}>Room</Text>
+        <Text style={styles.text}>{data.roomNumber}</Text>
+        <Text style={styles.label}>Guest</Text>
+        <Text style={styles.text}>None</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Room {data.roomNumber}</Text>
-      <Text style={styles.text}>Guest {data.guest?.name ?? "Unknown"}</Text>
+      <Avatar.Image
+        source={data.guest.avatar}
+        size={200}
+        style={styles.avatar}
+      />
+      <Text style={styles.label}>Room</Text>
+      <Text style={styles.text}>{data.roomNumber}</Text>
+      <Text style={styles.label}>Guest</Text>
+      <Text style={styles.text}>{data.guest.name}</Text>
+      <Text style={styles.label}>Requests</Text>
+      <FlatList style={styles.list}
+        data={data.guest.serviceRequests}
+        renderItem={({ item }) => <Text style={styles.text}>{item}</Text>}
+      />
     </SafeAreaView>
   );
 };
@@ -33,11 +57,22 @@ export const RoomDetailScreen = ({ navigation, route }: RoomDetailScreenProps) =
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alightItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    margin: 20
+  },
+  avatar: {
+    alignSelf: 'center',
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: '200',
   },
   text: {
-    textAlign: 'center',
-    fontSize: 40,
-  }
+    fontSize: 25,
+  },
+  list: {
+    width: '100%',
+  },
 });

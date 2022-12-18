822238a48af2bf14cf3dd786c5790ea58cab4a71
Michael L Perry <michael@qedcode.com>
2022-12-18 16:35:52 -0600

    Fixed params -> param

--- rooms/RoomsScreen.tsx
+++ rooms/RoomsScreen.tsx
@@ -3,15 +3,15 @@ import { LoadingScreen } from "@components/LoadingScreen";
 import { MaterialBottomTabScreenProps } from "@react-navigation/material-bottom-tabs";
 import { CompositeScreenProps } from '@react-navigation/native';
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StackContainerParamsList } from "../navigation/StackContainerParamsList";
-import { TabContainerParamsList } from "../navigation/TabContainerParamsList";
+import { StackContainerParamList } from "../navigation/StackContainerParamList";
+import { TabContainerParamList } from "../navigation/TabContainerParamList";
 import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { useRooms } from "./queries";
 import { RoomComponent } from "./RoomComponent";
 
 export type RoomsScreenProps = CompositeScreenProps<
-  MaterialBottomTabScreenProps<TabContainerParamsList, "Rooms">,
-  NativeStackScreenProps<StackContainerParamsList>
+  MaterialBottomTabScreenProps<TabContainerParamList, "Rooms">,
+  NativeStackScreenProps<StackContainerParamList>
 >;
 
 export const RoomsScreen = ({ navigation }: RoomsScreenProps) => {

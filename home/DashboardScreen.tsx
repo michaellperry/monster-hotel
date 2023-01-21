540791b61d3e019ff5551fcd41931fe254404546
Michael L Perry <michael@qedcode.com>
2023-01-21 15:52:01 -0600

    Replace buttons with dashboard items.

--- home/DashboardScreen.tsx
+++ home/DashboardScreen.tsx
@@ -1,32 +1,29 @@
 import { StackContainerScreenProps } from "@navigation/StackContainerParamList";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Button } from "react-native-paper";
+import { DashboardItem } from "./DashboardItem";
 
 type DashboardScreenProps = StackContainerScreenProps<"Dashboard">;
 
 export const DashboardScreen = ({ navigation }: DashboardScreenProps) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Button style={styles.button} contentStyle={styles.text} mode="contained" onPress={() => {
-          navigation.push("Tab", { screen: "Rooms" });
-        }}>
-        Rooms
-      </Button>
-      <Button style={styles.button} contentStyle={styles.text} mode="contained" onPress={() => {
-          navigation.push("Tab", { screen: "Requests" });
-        }}>
-        Requests
-      </Button>
-      <Button style={styles.button} contentStyle={styles.text} mode="contained" onPress={() => {
-          navigation.push("Tab", { screen: "Tasks" });
-        }}>
-        Tasks
-      </Button>
-      <Button style={styles.button} contentStyle={styles.text} mode="contained" onPress={() => {
-          navigation.push("Tab", { screen: "Alerts" });
-        }}>
-        Alerts
-      </Button>
+      <View style={{flexDirection: "row"}}>
+        <DashboardItem title="Rooms" icon="door-closed" value={2} onPress={() =>
+          navigation.push("Tab", { screen: "Rooms" })
+        } />
+        <DashboardItem title="Requests" icon="bell" value={1} onPress={() =>
+          navigation.push("Tab", { screen: "Requests" })
+        } />
+      </View>
+      <View style={{flexDirection: "row"}}>
+        <DashboardItem title="Tasks" icon="clipboard-list" value={3} onPress={() =>
+          navigation.push("Tab", { screen: "Tasks" })
+        } />
+        <DashboardItem title="Alerts" icon="alert" value={1} onPress={() =>
+          navigation.push("Tab", { screen: "Alerts" })
+        } />
+      </View>
       <Button style={styles.button} contentStyle={styles.text} mode="contained" onPress={() => {
           navigation.pop();
         }}>
@@ -40,9 +37,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     margin: 10,
+    width: 320,
   },
   text: {
     paddingVertical: 10,
